50fb75fdab632faeeadb951086c19cb24d72a9fc
Milan <milanm08@gmail.com>
2019-09-06 21:39:48 +0200

    Add StudentLineup

---
+++ Threading/Concepts/Concepts/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Concepts
+{
+    class Program
+    {
+        static bool done;
+        static readonly object locker = new object();
+
+        static void Main()
+        {
+            new Thread(Go).Start();
+            Go();
+
+            new Thread(() =>
+            {
+                Console.WriteLine("I'm running on another thread!");
+                Console.WriteLine("This is so easy!");
+            }).Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int temp = i;
+                new Thread(() => Console.Write(temp)).Start();
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Go()
+        {
+            lock (locker)
+            {
+                if (!done) { Console.WriteLine("Done"); done = true; }
+            }
+        }
+    }
+}
